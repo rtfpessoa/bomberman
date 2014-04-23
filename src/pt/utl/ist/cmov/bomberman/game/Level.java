0886d902c92b1af21e0bdf7c01b366e7d6943585
paulortm <paulortmarques@gmail.com>
2014-04-23 17:10:49 +0100

    Added getters and setters to Level

--- src/pt/utl/ist/cmov/bomberman/game/Level.java
+++ src/pt/utl/ist/cmov/bomberman/game/Level.java
@@ -24,4 +24,61 @@ public class Level {
 		this.pointsOpponent = pointsOpponent;
 		this.map = map;
 	}
+
+	public Integer getGameDuration() {
+		return gameDuration;
+	}
+
+	public void setGameDuration(Integer gameDuration) {
+		this.gameDuration = gameDuration;
+	}
+
+	public Integer getExplosionTimeout() {
+		return explosionTimeout;
+	}
+
+	public void setExplosionTimeout(Integer explosionTimeout) {
+		this.explosionTimeout = explosionTimeout;
+	}
+
+	public Integer getExplosionRange() {
+		return explosionRange;
+	}
+
+	public void setExplosionRange(Integer explosionRange) {
+		this.explosionRange = explosionRange;
+	}
+
+	public Integer getRobotSpeed() {
+		return robotSpeed;
+	}
+
+	public void setRobotSpeed(Integer robotSpeed) {
+		this.robotSpeed = robotSpeed;
+	}
+
+	public Integer getPointsRobot() {
+		return pointsRobot;
+	}
+
+	public void setPointsRobot(Integer pointsRobot) {
+		this.pointsRobot = pointsRobot;
+	}
+
+	public Integer getPointsOpponent() {
+		return pointsOpponent;
+	}
+
+	public void setPointsOpponent(Integer pointsOpponent) {
+		this.pointsOpponent = pointsOpponent;
+	}
+
+	public ArrayList<ArrayList<Character>> getMap() {
+		return map;
+	}
+
+	public void setMap(ArrayList<ArrayList<Character>> map) {
+		this.map = map;
+	}
+
 }
