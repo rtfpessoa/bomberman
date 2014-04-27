d0779de781930c8fb342f7ca08de500505641588
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2014-04-27 16:11:59 +0100

    we cannot hide nav in older versions, we must put it on low profile

--- src/pt/utl/ist/cmov/bomberman/controllers/FullScreenController.java
+++ src/pt/utl/ist/cmov/bomberman/controllers/FullScreenController.java
@@ -8,44 +8,42 @@ public class FullScreenController implements
 		View.OnSystemUiVisibilityChangeListener {
 
 	private final View rootView;
+	final int androidVersion = Build.VERSION.SDK_INT;
 
 	public FullScreenController(View rootView) {
 		this.rootView = rootView;
-		setImmersiveMode(true);
+		hideSystemUI();
 	}
 
 	@Override
 	public void onSystemUiVisibilityChange(int visibility) {
-		if (visibility == 0) {
-			mHideHandler.postDelayed(mHideRunnable, 2000);
+		if (androidVersion < Build.VERSION_CODES.KITKAT) {
+			mHideHandler.postDelayed(mHideRunnable, 5000);
 		}
 	}
 
-	private void setImmersiveMode(boolean enabled) {
+	private void hideSystemUI() {
 		int flags = 0;
-		final int version = Build.VERSION.SDK_INT;
 
-		if (version >= Build.VERSION_CODES.KITKAT) {
+		if (androidVersion >= Build.VERSION_CODES.KITKAT) {
 			flags = View.SYSTEM_UI_FLAG_IMMERSIVE
+					| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+					| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 					| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-					| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-					| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 					| View.SYSTEM_UI_FLAG_FULLSCREEN
-					| View.SYSTEM_UI_FLAG_LOW_PROFILE
-					| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-		} else if (version >= Build.VERSION_CODES.JELLY_BEAN) {
-			flags = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 					| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-					| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+					| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+					| View.SYSTEM_UI_FLAG_LOW_PROFILE;
+		} else if (androidVersion >= Build.VERSION_CODES.JELLY_BEAN) {
+			flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+					| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 					| View.SYSTEM_UI_FLAG_FULLSCREEN
-					| View.SYSTEM_UI_FLAG_LOW_PROFILE
-					| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-		} else if (version >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-			flags = View.SYSTEM_UI_FLAG_LOW_PROFILE
-					| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+					| View.SYSTEM_UI_FLAG_LOW_PROFILE;
+		} else if (androidVersion >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+			flags = View.SYSTEM_UI_FLAG_LOW_PROFILE;
 		}
 
-		if (version >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+		if (androidVersion >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
 			rootView.setSystemUiVisibility(flags);
 		}
 	}
@@ -54,7 +52,7 @@ public class FullScreenController implements
 	Runnable mHideRunnable = new Runnable() {
 		@Override
 		public void run() {
-			setImmersiveMode(true);
+			hideSystemUI();
 		}
 	};
 
