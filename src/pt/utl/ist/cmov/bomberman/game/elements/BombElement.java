cc72a411405efb7d7cc01b3ee4e15b28344babf0
Rafael Cortês <mrfyda@gmail.com>
2014-05-11 16:56:21 +0100

    more bomb

--- src/pt/utl/ist/cmov/bomberman/game/elements/BombElement.java
+++ src/pt/utl/ist/cmov/bomberman/game/elements/BombElement.java
@@ -1,6 +1,10 @@
 package pt.utl.ist.cmov.bomberman.game.elements;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import pt.utl.ist.cmov.bomberman.game.Level;
+import pt.utl.ist.cmov.bomberman.util.Direction;
 import pt.utl.ist.cmov.bomberman.util.Position;
 import android.os.Handler;
 import android.util.Log;
@@ -9,11 +13,11 @@ public class BombElement extends Element {
 
 	private BombermanElement bomberman;
 
-	private Handler bombHandler;
+	private Handler bombHandler = new Handler();
 	private Runnable bombRunnable;
 
 	private Boolean hasExploded;
-	private int[] efectiveRange;
+	private List<Position> positionsToExplode;
 
 	public BombElement(Level level, Integer id, Position pos,
 			BombermanElement bomberman) {
@@ -21,7 +25,8 @@ public class BombElement extends Element {
 
 		this.bomberman = bomberman;
 		this.hasExploded = false;
-		this.bombHandler = new Handler();
+		this.positionsToExplode = new LinkedList<Position>();
+
 		this.bombRunnable = new Runnable() {
 
 			@Override
@@ -46,113 +51,53 @@ public class BombElement extends Element {
 		return false;
 	}
 
-	private int[] explode() {
-		Position pos = this.getPos();
+	private void explode() {
+		Integer explosionRange = this.level.getExplosionRange();
 
-		boolean upSearchStop = false;
-		boolean downSearchStop = false;
-		boolean leftSearchStop = false;
-		boolean rightSearchStop = false;
+		positionsToExplode.add(this.getPos());
 
-		int explosionRange = this.level.getExplosionRange();
-		// indexes: 0 = Up; 1 = Down; 2 = Left; 3 = Right
-		int[] efectiveRange = { explosionRange, explosionRange, explosionRange,
-				explosionRange };
+		for (Direction direction : Direction.values()) {
+			Position pos = this.getPos();
 
-		this.level.putExploding(this, this.getPos());
-		for (int i = 1; i <= explosionRange; i++) {
-			if (!upSearchStop
-					&& this.level.getContent(pos.x, pos.y - i) != Level.WALL
-					&& this.level.getContent(pos.x, pos.y - i) != Level.EXPLODING
-					&& this.level.getContent(pos.x, pos.y - i) != Level.BOMB) {
-				if (this.level.getContent(pos.x, pos.y - i) != Level.EMPTY) {
-					upSearchStop = true;
-					efectiveRange[0] = i;
+			for (int i = 1; i <= explosionRange; i++) {
+				pos = Position.calculateNext(direction, pos);
+				Element element = this.level.getOnMap(pos);
+
+				if (isStopElementType(element.getType())) {
+					break;
+				} else if (element.getType() != Level.EMPTY) {
+					positionsToExplode.add(pos);
+					break;
+				} else {
+					positionsToExplode.add(pos);
 				}
-				this.level.putExploding(this, new Position(pos.x, pos.y - i));
-			} else if (!upSearchStop) {
-				upSearchStop = true;
-				efectiveRange[0] = i - 1;
-			}
-
-			if (!downSearchStop
-					&& this.level.getContent(pos.x, pos.y + i) != Level.WALL
-					&& this.level.getContent(pos.x, pos.y + i) != Level.EXPLODING
-					&& this.level.getContent(pos.x, pos.y + i) != Level.BOMB) {
-				if (this.level.getContent(pos.x, pos.y + i) != Level.EMPTY) {
-					downSearchStop = true;
-					efectiveRange[1] = i;
-				}
-				this.level.putExploding(this, new Position(pos.x, pos.y + i));
-			} else if (!downSearchStop) {
-				downSearchStop = true;
-				efectiveRange[1] = i - 1;
-			}
-
-			if (!leftSearchStop
-					&& this.level.getContent(pos.x - i, pos.y) != Level.WALL
-					&& this.level.getContent(pos.x - i, pos.y) != Level.EXPLODING
-					&& this.level.getContent(pos.x - i, pos.y) != Level.BOMB) {
-				if (this.level.getContent(pos.x - i, pos.y) != Level.EMPTY) {
-					leftSearchStop = true;
-					efectiveRange[2] = i;
-				}
-				this.level.putExploding(this, new Position(pos.x - i, pos.y));
-			} else if (!leftSearchStop) {
-				leftSearchStop = true;
-				efectiveRange[2] = i - 1;
-			}
-
-			if (!rightSearchStop
-					&& this.level.getContent(pos.x + i, pos.y) != Level.WALL
-					&& this.level.getContent(pos.x + i, pos.y) != Level.EXPLODING
-					&& this.level.getContent(pos.x + i, pos.y) != Level.BOMB) {
-				if (this.level.getContent(pos.x + i, pos.y) != Level.EMPTY) {
-					rightSearchStop = true;
-					efectiveRange[3] = i;
-				}
-				this.level.putExploding(this, new Position(pos.x + i, pos.y));
-			} else if (!rightSearchStop) {
-				rightSearchStop = true;
-				efectiveRange[3] = i - 1;
 			}
 		}
 
-		return efectiveRange;
+		for (Position pos : positionsToExplode) {
+			this.level.putExploding(this, pos);
+		}
 	}
 
-	private void finishExplosion(Position pos, int[] efectiveRange) {
-		this.level.putEmpty(pos);
+	private boolean isStopElementType(Character type) {
+		return type == Level.WALL || type == Level.EXPLODING
+				|| type == Level.BOMB;
+	}
 
-		// Up
-		for (int i = 1; i <= efectiveRange[0]; i++) {
-			this.level.putEmpty(new Position(pos.x, pos.y - i));
-		}
-
-		// Down
-		for (int i = 1; i <= efectiveRange[1]; i++) {
-			this.level.putEmpty(new Position(pos.x, pos.y + i));
-		}
-
-		// Left
-		for (int i = 1; i <= efectiveRange[2]; i++) {
-			this.level.putEmpty(new Position(pos.x - i, pos.y));
-		}
-
-		// Right
-		for (int i = 1; i <= efectiveRange[3]; i++) {
-			this.level.putEmpty(new Position(pos.x + i, pos.y));
+	private void finishExplosion() {
+		for (Position pos : positionsToExplode) {
+			this.level.putEmpty(pos);
 		}
 	}
 
 	private void bombTimer() {
 		if (!hasExploded) {
 			Log.i("BombExplosion", "Explosion");
-			this.efectiveRange = explode();
+			explode();
 			this.hasExploded = true;
 			bombHandler.postDelayed(bombRunnable, level.getExplosionDuration());
 		} else {
-			finishExplosion(this.getPos(), this.efectiveRange);
+			finishExplosion();
 		}
 	}
 
